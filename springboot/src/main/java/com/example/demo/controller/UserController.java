8195ee9359dfc0f96efcbcdb3bb3bce6600a714f
烂柯歧山 <1479039790@qq.com>
2022-09-02 17:20:15 +0800

    準備開始分頁

--- springboot/src/main/java/com/example/demo/controller/UserController.java
+++ springboot/src/main/java/com/example/demo/controller/UserController.java
@@ -20,7 +20,7 @@ public class UserController {
     @Resource//引入Mapper的userMapper
     UserMapper userMapper;
 
-    @PostMapping//定义post的接口
+    @PostMapping(value="/save")//定义post的接口
     public Result<?> save(@RequestBody User user){//RequestBody 可以将前台传来的json转化为User对象
         //Result<?>的问号表示所以类型的数据都可以接受
         if (user.getPassword()==null) {
@@ -29,19 +29,18 @@ public class UserController {
         userMapper.insert(user);
         return Result.success();//放回信息
     }
-
-//    @GetMapping
-//    public Result<?> findPage(@RequestParam(defaultValue = "1") Integer pageNum,@RequestParam(defaultValue = "10") Integer pageSize,@RequestParam(defaultValue = "") String search){
-//        //pageNum是当前页;pageSize是一页有多少条数据;search是查询的关键字(defaultValue是设置默认值)
-//        LambdaQueryWrapper<User> wrapper=Wrappers.<User>lambdaQuery();//sql条件的执行对象
-//        if (StrUtil.isNotBlank(search)){//StrUtil.isNotBlank(search)是hutool依赖用来帮助判断参数是否为空的
-//            wrapper.like(User::getNickName,search);
-//        }
-//        Page<User> userPage= userMapper.selectPage(new Page<>(pageNum,pageSize), wrapper);
-//        //selectPage需要Page对象和sql执行对象
-//        //new Page<>(pageNum,pageSize)//分页对象
-//        //Wrappers.<User>lambdaQuery().like(User::getNickName,search)//sql执行对象
-//        // User::getNickName是User.getNickName的变形
-//        return Result.success(userPage);//Result.success(变量名)//用返回变量
-//    }
+    @GetMapping(value = "/findPage")
+    public Result<?> findPage(@RequestParam(defaultValue = "1") Integer pageNum,@RequestParam(defaultValue = "10") Integer pageSize,@RequestParam(defaultValue = "") String search){
+        //pageNum是当前页;pageSize是一页有多少条数据;search是查询的关键字(defaultValue是设置默认值)
+        LambdaQueryWrapper<User> wrapper=Wrappers.<User>lambdaQuery();//sql条件的执行对象
+        if (StrUtil.isNotBlank(search)){//StrUtil.isNotBlank(search)是hutool依赖用来帮助判断参数是否为空的
+            wrapper.like(User::getNickName,search);
+        }
+        Page<User> userPage= userMapper.selectPage(new Page<>(pageNum,pageSize), wrapper);
+        //selectPage需要Page对象和sql执行对象
+        //new Page<>(pageNum,pageSize)//分页对象
+        //Wrappers.<User>lambdaQuery().like(User::getNickName,search)//sql执行对象
+        // User::getNickName是User.getNickName的变形
+        return Result.success(userPage);//Result.success(变量名)//用返回变量
+    }
 }
---
+++ vue/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <!-- 头部-->
+    <Header /><!-- components文件夹中的组件，用的时候就导入然后用标签方式使用-->
+
+    <!-- 主体-->
+    <div style="display: flex">
+      <!--侧边栏-->
+      <Aside/>
+      <!-- 内容区域-->
+      <router-view style="flex: 1" />
+    </div>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+import Header from "@/components/Header";
+import Aside from "@/components/Aside";
+
+export default {
+  name:"Layout",
+  components:{
+    Header,/* Header是组件名，写了export default后，选中组件名按Alt+Enter生成 import Header from "@/components/Header";*/
+    Aside
+  }
+}
+
+</script>
